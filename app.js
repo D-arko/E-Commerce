da87fead173924da57b52fe67d719d4595e8b892
Dar-ko <darko.velickovic1@gmail.com>
2023-05-11 15:24:27 +0200

    backend finished

--- app.js
+++ app.js
@@ -2,21 +2,15 @@ const express = require('express');
 const app = express();
 const session = require("express-session");
 const PORT = 3000;
-const Pool = require('pg').Pool;
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const store = new session.MemoryStore();
+const db = require('./queries.js');
+
 const bodyParser = require('body-parser');
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'E-Commerce-db1',
-    password: 'postgres',
-    port: 5432
-});
-
 app.use(bodyParser.json());
+
 
 app.use(
   session({
@@ -46,152 +40,63 @@ passport.use(new LocalStrategy(function verify(username, password, cb) {
     });
 }));
 
-// USERS
-// Get all users from a database
-const getAllUsers = (request, response) => {
-    pool.query('SELECT * FROM users;', (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).json(results.rows);
-        }
-    });
-};
-
-// Get a single user from a database by id
-const getUser = (request, response) => {
-    const id = parseInt(req.params.id);
-
-    pool.query('SELECT * FROM users WHERE id = $1', [ id ], (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).json(results.rows);
-        }
-    });
-};
-
-// Registering new user
-const registerUser = (request, response) => {
-    const { username, password } = request.body;
-    
-    pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [ username, password ], (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).send(`User created with username: ${username}.`);
-        }
-    });
-};
-
-// Delete user from database by id
-const deleteUser = (request, response) => {
-    const id = parseInt(req.params.id);
-
-    pool.query('DELETE * FROM users WHERE id = $1', [ id ], (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).send(`User deleted with id = ${id}`);
-        }
-    });
-};
-
-
-// PRODUCTS
-// Get all products
-const getAllProducts = (request, response) => {
-    pool.query('SELECT * FROM products ORDER BY id ASC;', (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).json(results.rows);
-        }
-    });
-};
-
-// Get a single product by id
-const getProduct = (request, response) => {
-    const id = parseInt(request.params.id);
-
-    pool.query('SELECT * FROM products WHERE id = $1'), [ id ], (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).json(results.rows);
-        }
-    };
-};
-
-// Insert new product into database
-const insertProduct = (request, response) => {
-    const { name, category, quantity_available } = req.body;
-    
-    pool.query('INSERT INTO products (name, category, quantity_available) VALUES ($1, $2, $3) RETURNING *;', 
-        [ name, category, quantity_available ], (error, results) => {
-            if (error) {
-                throw error;
-            } else {
-                response.status(201).json(results.rows);
-            }
-        });
-    };
-    
-// Delete product by id
-const deleteProduct = (request, response) => {
-    const id = parseInt(req.params.id);
-
-    pool.query('DELETE FROM products WHERE id = $1', [ id ], (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            response.status(200).send(`User deleted with id ${id}`);
-        }
-    });
-};
-
-
-// Logging in a user
-app.post('/login',
-passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-function(req, res) {
-    res.redirect('/~' + req.user.username);
-}
-);
-
-// Render login page
-app.get('/login',
-function(req, res, next) {
-    res.render('login');
+        // ROUTES //
+// homepage
+app.get('/', (request, response) => {
+    response.send('Welcome to E-Commerce!');
 });
 
-// PRODUCTS ROUTES
+    // LOG IN
+// Route for logging in a user
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+    (request, response) => {
+        response.redirect('/~' + request.user.username);
+});
+
+// Render login page
+app.get('/login', (request, response) => {
+    response.render('login');
+});
+
+    // PRODUCTS ROUTES
 // Route for handling requests for getting all products
-app.get('/products', getAllProducts);
+app.get('/products', db.getAllProducts);
 // Route for handling requests for getting a product by id
 
-app.get('/products/:id', getProduct);
+app.get('/products/:id', db.getProduct);
 
 // Route for handling requests for inserting a new product
-app.post('/products', insertProduct);
+app.post('/products', db.insertProduct);
 
 // Route for handling requests for deleting a product by id
-app.delete('/products/:id', deleteProduct);
+app.delete('/products/:id', db.deleteProduct);
 
-// USERS ROUTES
+        // USERS ROUTES
 // Route for handling requests for getting all users
-app.get('/users', getAllUsers);
+app.get('/users', db.getAllUsers);
 
 // Route for handling request for getting a single user by id
-app.get('/users/:id', getUser);
+app.get('/users/:id', db.getUser);
 
 // Route for handling requests for registering new users
-app.post('/users/register', registerUser);
+app.post('/users/register', db.registerUser);
 
-// Route for  handling requests for deleting a user by id
-app.delete('/users/:id', deleteUser);
+// Route for handling requests for deleting a user by id
+app.delete('/users/:id', db.deleteUser);
+
+    // CART ROUTES
+// Route for handling requests for getting all carts
+app.get('/cart', db.getAllCarts);
+
+// Route for handling requests for getting a cart by id
+app.get('/cart/:id', db.getCart);
+
+// Route for handling requests for inserting a new product into cart
+app.post('/cart', db.addToCart);
+
 
 // listening port
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+    console.log(`Server listening on PORT: ${PORT}`);
 });
