87354d736f89993b2e2a670989081908c706ab05
D-arko <113909577+D-arko@users.noreply.github.com>
2025-02-09 09:21:51 +0100

    delete all passport routes

--- app.js
+++ app.js
@@ -2,64 +2,19 @@ const express = require('express');
 const app = express();
 const session = require("express-session");
 const PORT = 3000;
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const store = new session.MemoryStore();
 const db = require('./queries.js');
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-
-app.use(
-  session({
-    secret: "f4z4gs$Gcg",
-    cookie: { maxAge: 300000000, secure: false },
-    saveUninitialized: false,
-    resave: false,
-    store,
-})
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Strategy for loging users in
-passport.use(new LocalStrategy(function verify(username, password, cb) {
-    db.get('SELECT * FROM users WHERE usename = ?', [ username ], function(err, user) {
-        
-        // error occured
-        if (err) { return cb(err); }
-        
-        // no error occured, but user doesn't exist or password is incorrect
-        if (!user) { return cb(null, false, { message: 'Incorrect username or password.'}); }
-        
-        // no error occured, user found and password is correct
-        return cb(null, user);
-    });
-}));
-
-        // ROUTES //
+// ROUTES //
 // homepage
 app.get('/', (request, response) => {
     response.send('Welcome to E-Commerce!');
 });
 
-    // LOG IN
-// Route for logging in a user
-app.post('/login',
-    passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-    (request, response) => {
-        response.redirect('/~' + request.user.username);
-});
-
-// Render login page
-app.get('/login', (request, response) => {
-    response.render('login');
-});
-
-    // PRODUCTS ROUTES
+// PRODUCTS ROUTES
 // Route for handling requests for getting all products
 app.get('/products', db.getAllProducts);
 // Route for handling requests for getting a product by id
